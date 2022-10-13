5aaf7e3c719d6038bc813e4022e0e9b87bd1f88f
D Johnson <DJohnson@folkestonegirls.kent.sch.uk>
2022-10-13 17:43:32 +0100

    Added IProblem Interface

---
+++ Abstractions/IProblem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEuler.Abstractions
+{
+    public interface IProblem
+    {
+        string Name { get; set; }
+        string Answer { get; set; }
+        void Solve();
+        void Print(object answer);
+    }
+}
